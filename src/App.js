94ded65d028d77c3096348efa04200c17fff74d7
tomoa112 <78849244+tomoa112@users.noreply.github.com>
2021-12-19 23:31:42 +0900

    Add Style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 
 // React では関数をコンポーネントに反映できる。アロー関数をかく。
 const App = () => {
+  // onClickButton 関数でボタン押された後のアクションを記載する。alert()など
   const onClickButton = () => alert();
-
+  // css（スタイル）は、以下のように表記する。
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px" // ふつうは font-size と書くが、Reactの場合キャメルケースで書く。
+  };
   // return で返す内容にHTMLを書いていく。関数内にHTMLを書く書き方がJSX記法と呼ぶ。
   // return の内容は必ず一つのタグで囲う必要がある（例：<div></dev>内に複数のタグを入れるなど)
   return (
@@ -11,10 +16,13 @@ const App = () => {
     //  React.Fragmentタグで囲むとこのFragmentタグはレンダリングされない。
     //  <></>で囲んでもReact.Fragmentタグと同様のタグとして扱われる。
     <React.Fragment>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-      <button onClick={onClickButton}></button>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
+    // ・Style は、style={{}} の外側｛｝はJavascriptの関数用の内側の｛｝がスタイル記載用。
+    //		color: 'red' などプロパティ値は文字列として記載必要。
+    // ・イベント（ボタンクリックなど）は、関数名を｛｝内に記載して押したら呼ばれるようにする。
   );
 };
 export default App; // Appファイル（コンポーネント）をほかのJSファイルで使えるようにする。
--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,6 @@
 import React from "react"; //React17まではJSXを使う時にお作法でつけたが、それ以降は任意。
 import ReactDom from "react-dom"; // html にコンポーネントを反映するもの
-//import App from "./App"; // src配下の Appファイルから関数を読み込む。
-
-// React では関数をコンポーネントに反映できる。アロー関数をかく。
-const App = () => {
-  const onClickButton = () => alert();
-
-  // return で返す内容にHTMLを書いていく。関数内にHTMLを書く書き方がJSX記法と呼ぶ。
-  // return の内容は必ず一つのタグで囲う必要がある（例：<div></dev>内に複数のタグを入れるなど)
-  return (
-    // <div>で囲むとエラー回避のためだけにHTMLにDivタグがレンダリングされるので、
-    //  React.Fragmentタグで囲むとこのFragmentタグはレンダリングされない。
-    //  <></>で囲んでもReact.Fragmentタグと同様のタグとして扱われる。
-    <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-      <button onClick={onClickButton}></button>
-    </>
-  );
-};
+import App from "./App"; // src配下の Appファイルから関数を読み込む。
 
 // html に反映するために、render関数を使う。
 ReactDom.render(<App />, document.getElementById("root"));
