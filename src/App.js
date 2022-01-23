f8f98f4cc8ec5b8b22a257ec85cb1a421c82db6d
tomoa112 <78849244+tomoa112@users.noreply.github.com>
2022-01-23 10:05:56 +0900

    add useState useState を使って、動的に変数などを変更

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+// １        ★useState を使うときはここを指定
+import React, { useState } from "react";
 import ColorfulMessage from "./Components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  // ５★numの状態変更のsetNum に num+1 を入れることで、動的に＋１の数値を入れられる。
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  // ２★変数に状態を持たせる。num が変数名、setNum がnum の状態を変更する関数名。
+  // ★さらに分割代入で useState. と参照しなくていいように指定する。
+  // ★useState()に値を持たせることで、状態の初期値を持たせられる。
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      {/* ★HTMLのコメントを JavaScript として｛｝で囲む。ColorfulMessageコンポーネントへprops の color と messageを指定する。 */}
       <ColorfulMessage color="blue" message="お元気ですか？" />
-      {/* ★コンポーネントもタグのように文字列を囲んでその文字列を引数にできる */}
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      {/* ４★num はJavaScriptにするため、｛｝で囲む。ここで表示できる */}
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      {/* ３★num はJavaScriptにするため、｛｝で囲む。ここで表示できる */}
+      <p>{num}</p>
     </>
   );
 };
