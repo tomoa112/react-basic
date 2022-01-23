4ee4d3ed1143994a6f8f15c7b8d90a311bc48140
tomoa112 <78849244+tomoa112@users.noreply.github.com>
2022-01-23 15:11:52 +0900

    add useEffect Stateが複数ある場合の影響について確認

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-// １        ★useState を使うときはここを指定
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+// useEffect を追加
+import React, { useState, useEffect } from "react";
 import ColorfulMessage from "./Components/ColorfulMessage";
-
 const App = () => {
-  // ５★numの状態変更のsetNum に num+1 を入れることで、動的に＋１の数値を入れられる。
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  // ２★変数に状態を持たせる。num が変数名、setNum がnum の状態を変更する関数名。
-  // ★さらに分割代入で useState. と参照しなくていいように指定する。
-  // ★useState()に値を持たせることで、状態の初期値を持たせられる。
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  // ★useEffect の第2引数に [] の空の配列を指定すると、最初の画面表示の時だけ
+  //  useEffect内が実行される。そのため、初期値を最初の1回だけ設定したい場合には、ここに設定する。
+  //useEffect(() => {
+  //  console.log("useEffect!!");
+  //}, []);
+
+  // ★useEffect の中に num を入れることで、num の状態が変化した時だけ、useEffect内の文
+  // （num）の状態によって、変化させたい構文を入れる）が実行される。
+  // この構文の場合、第2引数の num に波線の注意書きが表示され、Codesandboxの eslint が以下の注意を出し、useEffect内で使用しているもう一つの状態変数 faceShowFlag も第2引数に設定すべきでは？と怒られる。ただ、今回は、faceShowFlagと分離したいので、指定する必要なし。正常に実行も可能。
+  // React Hook useEffect has a missing dependency: 'faceShowFlag'. Either include it or remove the dependency array. (react-hooks/exhaustive-deps)eslint
+  useEffect(() => {
+    console.log("useEffect!!");
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue" message="お元気ですか？" />
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
-      {/* ４★num はJavaScriptにするため、｛｝で囲む。ここで表示できる */}
       <button onClick={onClickCountUp}>カウントアップ</button>
-      {/* ３★num はJavaScriptにするため、｛｝で囲む。ここで表示できる */}
+      <button onClick={onClickSwitchShowFlag}>On/Off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ﾟДﾟ)</p>}
     </>
   );
 };
